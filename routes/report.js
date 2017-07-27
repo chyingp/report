2a3dc6a585425b6b41cdddf5cabfc270d99ea49a
casperchen(陈映平) <chyingp@gmail.com>
2017-07-27 11:28:30 +0800

    完善上报逻辑

--- routes/report.js
+++ routes/report.js
@@ -3,16 +3,68 @@ var router = express.Router();
 var bunyan = require('bunyan');
 var path = require('path');
 
+// TODO 是否把CGI ERROR 打印到独立的文件里，方便统计错误数？
+// 上报类型
+var TYPE_CGI_SPEED = 'cgi_speed';
+var TYPE_PAGE_SPEED = 'page_speed';
+var TYPE_PAGE_VIEW = 'page_view';
+var TYPE_DEFAULT = 'default';
+
+// 日志文件名
+var LOG_FILE_NAME_CGI_SPEED = 'cgi-speed.log';
+var LOG_FILE_NAME_PAGE_SPEED = 'page-speed.log';
+var LOG_FILE_NAME_PAGE_VIEW = 'page-view.log';
+var LOG_FILE_NAME_DEFAULT = 'default.log';
+
+var createLogger = function (type) {
+    // var type = item.type;
+    var filename;
+    switch(type) {
+        case TYPE_CGI_SPEED: filename = LOG_FILE_NAME_CGI_SPEED; break;
+        case TYPE_PAGE_SPEED: filename = LOG_FILE_NAME_PAGE_SPEED; break;
+        case TYPE_PAGE_VIEW: filename = LOG_FILE_NAME_PAGE_VIEW; break;
+        default: filename = LOG_FILE_NAME_DEFAULT; break;
+    }
+    
+    var logger = bunyan.createLogger({
+        name: "myapp",
+        streams: [{
+            type: 'rotating-file',
+            path: path.join(__dirname, '../logs/', filename),
+            period: '1d',   // daily rotation
+            count: 3        // keep 3 back copies
+        }]
+    });
+
+    return logger;
+};
+
+var pageViewLogger = createLogger(TYPE_PAGE_VIEW);
+var pageSpeedLogger = createLogger(TYPE_PAGE_SPEED);
+var cgiSpeedLogger = createLogger(TYPE_CGI_SPEED);
+var defaultLogger = createLogger(TYPE_DEFAULT);
+
+var getLogger = function (type) {
+    var logger;
+    switch(type) {
+        case TYPE_CGI_SPEED: logger = cgiSpeedLogger; break;
+        case TYPE_PAGE_SPEED: logger = pageSpeedLogger; break;
+        case TYPE_PAGE_VIEW: logger = pageViewLogger; break;
+        default: filename = logger = defaultLogger; break;
+    }
+    return logger;
+};
+
 // 日志输出配置
-var logger = bunyan.createLogger({
-    name: "myapp",
-    streams: [{
-        type: 'rotating-file',
-        path: path.join(__dirname, '../logs/speed-report.log'),
-        period: '1d',   // daily rotation
-        count: 3        // keep 3 back copies
-    }]
-});
+// var logger = bunyan.createLogger({
+//     name: "myapp",
+//     streams: [{
+//         type: 'rotating-file',
+//         path: path.join(__dirname, '../logs/speed-report.log'),
+//         period: '1d',   // daily rotation
+//         count: 3        // keep 3 back copies
+//     }]
+// });
 
 // TODO 上报过滤器 - P0
 var filters = {
@@ -22,10 +74,9 @@ var filters = {
 };
 
 // TODO 过滤无效的请求（比如恶意请求？）- P1
-
 // TODO hostname 改成ip（结合nginx）- P1
-
 // TODO 时间戳改成本地时间（当前用的是格林威治时间）- P1 -- done
+// TODO 支持跨域 P1
 
 var fmDate = function (date) {
     var arr = [
@@ -38,14 +89,17 @@ var fmDate = function (date) {
     // return [date.getFullYear(), date.getMonth() + 1, date.getDate()].join('-') + ' ' + [date.getHours(), date.getMinutes(), date.getSeconds()].join(':');    
 };
 
-var addSpeedRouter = function (req, res, next) {
+var addReportRouter = function (req, res, next) {
     var opt = req.method === 'GET' ? req.query : req.body;
     opt.time = fmDate(new Date());
+
+    var logger = getLogger(opt.type);
     logger.info(opt);
+
     res.end('ok');    
 };
 
-router.get('/add', addSpeedRouter);
-router.get('/post', addSpeedRouter);
+router.get('/add', addReportRouter);
+router.get('/post', addReportRouter);
 
 module.exports = router;
